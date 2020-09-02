75a5f83482468c7e34f5136532912e5012326623
Pratik Sachaniya <34182240+pratiksachaniya@users.noreply.github.com>
2020-09-02 09:36:22 +0530

    Add files via upload

---
+++ helloword.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+  main()
+ {
+    char ar[20];
+    char ar2[20];
+     cout << "hello word";
+
+     cin >> ar;
+     cout << endl;
+     cin >> ar2;
+
+     cout << ar << endl << ar2;
+ }
---
+++ sum.cpp
@@ -0,0 +1,10 @@
+#include <iostream.h>
+#include <conio.h>
+   void main()
+{
+    clrscr();
+    int a,b;
+
+    cout<<"Enter Number 1: ";
+    getch();
+}
